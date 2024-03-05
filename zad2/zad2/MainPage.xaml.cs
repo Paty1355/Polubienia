7792a063c8bfcc5972cc7a81cf394fc8c3a36998
Paty1355 <94170413+Paty1355@users.noreply.github.com>
2024-03-05 21:45:57 +0100

    Dodanie plikow

---
+++ zad2/zad2/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace zad2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        int like = 0;
+        public void polubienie(object sender, EventArgs e)
+        {
+            like++;
+            result.Text = like.ToString() + " polubień";
+        }
+        public void depolubienie(object sender, EventArgs e)
+        {
+            if(like > 0)
+                    like--;
+            result.Text = like.ToString() + " polubień";
+        }
+    }
+}
